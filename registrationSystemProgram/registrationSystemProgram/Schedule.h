aaf29c17716ea8a436c8e67fa4e498ea8e4b8079
LuisSanchezAlvarez <luis.sanchez.alvarez@est.una.ac.cr>
2025-01-12 23:24:46 -0600

    Create Schedule.h

---
+++ registrationSystemProgram/registrationSystemProgram/Schedule.h
@@ -0,0 +1,53 @@
+#pragma once
+
+class Schedule {
+private:
+	std::string day;
+	std::string startTime;
+	std::string endTime;
+	std::string classRoom;
+
+public:
+	Schedule();
+	~Schedule();
+
+	std::string getDay();
+	std::string getStartTime();
+	std::string getEndTime();
+	std::string getClassRoom();
+	void setDay(std::string day);
+	void setStartTime(std::string startTime);
+	void setEndTime(std::string endTime);
+	void setClassRoom(std::string classRoom);
+
+};
+
+Schedule::Schedule() {}
+
+Schedule::~Schedule() {}
+
+std::string Schedule::getDay() {
+	return this->day;
+}
+std::string Schedule::getStartTime() {
+	return this->startTime;
+}
+std::string Schedule::getEndTime() {
+	return this->endTime;
+}
+std::string Schedule::getClassRoom() {
+	return this->classRoom;
+}
+
+void Schedule::setDay(std::string day) {
+	this->day = day;
+}
+void Schedule::setStartTime(std::string startTime) {
+	this->startTime = startTime;
+}
+void Schedule::setEndTime(std::string endTime) {
+	this->endTime = endTime;
+}
+void Schedule::setClassRoom(std::string classRoom) {
+	this->classRoom = classRoom;
+}
