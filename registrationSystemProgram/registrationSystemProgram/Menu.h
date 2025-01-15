0f42ce915c58dc24cf0da7caba8f77233e421d3c
LuisSanchezAlvarez <luis.sanchez.alvarez@est.una.ac.cr>
2025-01-14 23:56:37 -0600

    Update Menu.h

--- registrationSystemProgram/registrationSystemProgram/Menu.h
+++ registrationSystemProgram/registrationSystemProgram/Menu.h
@@ -1,6 +1,6 @@
 #pragma once
 //#include <iostream>
-void subMenuA() {
+bool subMenuA() {
 	std::cout << "Archivo:" << std::endl;
 	std::cout << "  (a)Acerca de\n"<<
 		"  (b)Salir " << std::endl;
@@ -9,17 +9,17 @@ void subMenuA() {
 	std::cin >> opcion;
 	switch (opcion) {
 	case 'a':
-		std::cout << "aopcion1" << std::endl;
-		break;
+		about();
+		return false;
 	case 'b':
-		std::cout << "aopcion2" << std::endl;
-		break;
+		std::cout << "Saliendo....." << std::endl;
+		return true;
 	default:
-		break;
+		return false;
 	}
 }
 
-void subMenuB() {
+void subMenuB(Registration** registrationList, int rows, int columns, int recordedAmount) {
 	std::cout << "Mantenimiento:" << std::endl;
 	std::cout << "  (a)Estudiantes\n" <<
 		"  (b)Cursos \n" <<
@@ -42,7 +42,7 @@ void subMenuB() {
 	}
 }
 
-void subMenuC() {
+void subMenuC(Registration** registrationList, int rows, int columns, int recordedAmount) {
 	std::cout << "Matricula:" << std::endl;
 	std::cout << "  (a)Registro de matricula" << std::endl;
 	std::cout << "Selecione una opcion:" << std::endl;
@@ -57,19 +57,23 @@ void subMenuC() {
 	}
 }
 
-void subMenuD() {
+void subMenuD(Registration** registrationList, int rows, int columns, int recordedAmount, Course* courseList, int numCourse, Student* studentList, int numStudents) {
 	std::cout << "Consulta:" << std::endl;
-	std::cout << "  (a)Estudiantes\n" <<
-		"  (b)Cursos \n" << std::endl;
+	std::cout << "  (a)Estudiantes(Lista de estudiantes)\n" <<
+		"  (b)Cursos(Lista de los cursos)\n" <<
+		"  (c)Estudiantes Matriculados(Todos los matriculados) \n" << std::endl;
 	std::cout << "Selecione una opcion:" << std::endl;
 	char opcion = ' ';
 	std::cin >> opcion;
 	switch (opcion) {
 	case 'a':
-		std::cout << "bopcion1" << std::endl;
+		showStudentsList(studentList,numStudents);
 		break;
 	case 'b':
 		std::cout << "bopcion2" << std::endl;
+		break;
+	case 'c':
+		showEnrolledStudents(registrationList, rows, columns, recordedAmount);
 		break;
 	default:
 		break;
@@ -77,24 +81,33 @@ void subMenuD() {
 }
 
 
-void menu() {
-	std::cout<< "Bienvenido al sistema de registro o matricula" << std::endl;
-	std::cout << "  (a)Archivo\n" <<
-		"  (b)Mantenimiento \n" <<
-		"  (c)Matricula\n" <<
-		"  (d)Consulta\n" << std::endl;
-	std::cout << "Ingrese la opcion que desea realizar: " << std::endl;
-	char opcion=' ';
-	std::cin >> opcion;
-	switch (opcion){
-	case 'a':
-		subMenuA();
-		break;
-	case 'b':
-		subMenuB();
-		break;
-	default:
-		break;
+void menu(Registration** registrationList, int rows, int columns, int recordedAmount,Course* courseList,int numCourse,Student* studentList,int numStudents) {
+	bool exit = false;
+	while (exit==false) {
+		std::cout << "\nBienvenido al sistema de registro o matricula" << std::endl;
+		std::cout << "  (a)Archivo\n" <<
+			"  (b)Mantenimiento \n" <<
+			"  (c)Matricula\n" <<
+			"  (d)Consulta\n" << std::endl;
+		std::cout << "Ingrese la opcion que desea realizar: " << std::endl;
+		char opcion = ' ';
+		std::cin >> opcion;
+		switch (opcion) {
+		case 'a':
+			exit=subMenuA();
+			break;
+		case 'b':
+			subMenuB(registrationList, rows, columns, recordedAmount);
+			break;
+		case 'c':
+			subMenuC(registrationList, rows, columns, recordedAmount);
+			break;
+		case 'd':
+			subMenuD(registrationList,rows,columns,recordedAmount,courseList,numCourse,studentList,numStudents);
+			break;
+		default:
+			break;
+		}
 	}
 
 }
