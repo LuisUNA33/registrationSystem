cd2fd39529829cb1066509fc8c6c1bef28f8c591
LuisSanchezAlvarez <luis.sanchez.alvarez@est.una.ac.cr>
2025-01-14 23:56:51 -0600

    Update registrationSystemProgram.cpp

--- registrationSystemProgram/registrationSystemProgram/registrationSystemProgram.cpp
+++ registrationSystemProgram/registrationSystemProgram/registrationSystemProgram.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+
 #include "Student.h"
+#include "Schedule.h"
+#include "Course.h"
+#include "Registration.h"
+
+#include "processes.h"
+#include "Menu.h"
+
+#include "loadPreRecords.h"
+
 
 int main(){
+    int columns = 10;
+    int rows = 10;
+    Registration **registrationList=initializeArray(rows,columns);
+    int recordedAmount=0;
 
-    std::cout << "Hello World!\n";
-    Student student1;
-    student1.setName("Luka");
+    Student *studentList = new Student[rows+columns];
+    int numStudents = 0;
 
-    std::cout << student1.getName()<< std::endl;
+    Course* courseList = new Course[10];
+    int numCourses = 0;
 
+    loadPreRecords(registrationList,recordedAmount,courseList,numCourses,studentList,numStudents);
+    std::cout <<"Cantidad de matriculados previamente: " << recordedAmount << std::endl;
+    std::cout << "Cantidad de Cursos creados: " << numCourses << std::endl;
+    std::cout << "Cantidad de Estudiantes: " << numStudents << std::endl;
+    
+    menu(registrationList,rows,columns, recordedAmount, courseList, numCourses, studentList, numStudents);
+    std::cout << "Fin del Programa" << std::endl;
+    
 
+    return 0;
 }
