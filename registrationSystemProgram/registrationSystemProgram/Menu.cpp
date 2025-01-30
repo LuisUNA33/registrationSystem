8845429bc69797102a05add3cf336ae3edb79ad2
Mathew <163693332+mathewramirez@users.noreply.github.com>
2025-01-30 13:26:18 -0600

    modified of menu and texting forms

---
+++ registrationSystemProgram/registrationSystemProgram/Menu.cpp
@@ -0,0 +1,173 @@
+#include "Menu.h"
+bool subMenuA() {
+    char option;
+    while (true) {
+        system("CLS");
+        std::cout << "=== Archivo ===" << std::endl;
+        std::cout << "a) Acerca de" << std::endl;
+        std::cout << "b) Salir del Programa" << std::endl;
+        std::cout << "c) Ir al Menu principal" << std::endl;
+        std::cout << "Ingrese alguna opcion disponible: " << std::endl;
+        std::cin >> option;
+
+        option = std::tolower(option);
+
+        switch (option) {
+        case 'a':
+            about();
+            return false;
+        case 'b':
+            std::cout << "Saliendo....." << std::endl;
+            return true;
+        case 'c':
+            return;
+        default:
+            return false;
+        }
+    }
+}
+
+void subMenuB(Course* courseList, int& numCourse,Student* studentList, int& numStudents,Schedule* scheduleList, int& numSchedules) {
+    system("CLS");
+    char option;
+    while (true) {
+            system("CLS");
+            std::cout << "=== Mantenimiento ===" << std::endl;
+            std::cout << "a) Estudiantes" << std::endl;
+            std::cout << "b) Cursos" << std::endl;
+            std::cout << "c) Horarios" << std::endl;
+            std::cout << "d) regresar " << std::endl;
+            std::cout << "Ingrese alguna opcion disponible: " << std::endl;
+            std::cin >> option;
+            if (std::cin.fail() || std::cin.peek() != '\n') {
+                std::cout << "Error: entrada no valida." << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+
+        char option = enterChar();
+        switch (option) {
+        case 'a':
+            system("CLS");
+            registerStudent(studentList, numStudents);
+            break;
+        case 'b':
+            system("CLS");
+            registerCourse(courseList, numCourse, scheduleList, numSchedules);
+            break;
+        case 'c':
+            system("CLS");
+            registerSchedule(scheduleList, numSchedules);
+            break;
+        case 'd':
+            return;
+        default:
+            break;
+        }
+    }
+}
+
+void subMenuC(Registration* registrationList, int& numRegistration,
+	Student* studentList, int numStudents, Course* courseList, int numCourse, Schedule* scheduleList, int numSchedules) {
+    char option;
+    do {
+        std::cout << "Matricula:" << std::endl;
+        std::cout << "  (a)Registro de matricula" << std::endl;
+        std::cout << "  (b)Registro de matricula" << std::endl;
+        std::cout << "  (c) regresar " << std::endl;
+        std::cout << "Selecione una opcion:" << std::endl;
+        option = enterChar();
+        switch (option) {
+        case 'a':
+            addRegistration(registrationList, numRegistration,
+                studentList, numStudents, courseList, numCourse, scheduleList, numSchedules);
+        case 'b':
+            return;
+
+        default:
+            break;
+        }
+    } while (option != 'a' || option != 'b' || option != 'c');
+
+}
+
+
+void subMenuD(Registration* registrationList, int numRegistration,
+	Course* courseList, int numCourse,
+	Student* studentList, int numStudents,
+	Schedule* scheduleList, int numSchedules) {
+	std::cout << "Consulta:" << std::endl;
+    system("CLS");
+    char option;
+    while (true) {
+        std::cout << "  (a)Estudiantes(Lista de estudiantes)" << std::endl;
+        std::cout << "  (b)Cursos(Lista de los cursos)"<< std::endl;
+        std::cout << "  (c)Horarios(Lista de Horarios)" << std::endl;
+        std::cout << "  (d)Estudiantes Matriculados(Todos los matriculados)" << std::endl;
+        std::cout << "  (e)regresar" << std::endl;
+	std::cout << "Selecione una opcion:" << std::endl;
+	option = enterChar();
+	switch (option) {
+	case 'a':
+		showStudentsList(studentList, numStudents);
+		break;
+	case 'b':
+		showCourseList(courseList, numCourse);
+		break;
+	case 'c':
+		showScheduleList(scheduleList, numSchedules);
+		break;
+	case 'd':
+		showEnrolledStudents(registrationList, numRegistration);
+		break;
+	default:
+		break;
+	}
+    }
+}
+
+
+void menu(Registration* registrationList, int& numRegistration,
+	Course* courseList, int& numCourse,
+	Student* studentList, int& numStudents,
+	Schedule* scheduleList, int& numSchedules) {
+	bool exit = false;
+	while (exit == false) {
+		std::cout << "\nBienvenido al sistema de registro o matricula" << std::endl;
+		std::cout << "  (a)Archivo\n" <<
+			"  (b)Mantenimiento \n" <<
+			"  (c)Matricula\n" <<
+			"  (d)Consulta\n" << std::endl;
+		std::cout << "Ingrese la opcion que desea realizar: " << std::endl;
+		char option = enterChar();
+		//char option =' ';
+		//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		//std::cin >> option;
+		switch (option) {
+		case 'a':
+			exit = subMenuA();
+			break;
+		case 'b':
+			subMenuB(courseList, numCourse, studentList, numStudents, scheduleList, numSchedules);
+			break;
+		case 'c':
+			subMenuC(registrationList, numRegistration,
+				studentList, numStudents, courseList, numCourse, scheduleList, numSchedules);
+			break;
+		case 'd':
+			subMenuD(registrationList, numRegistration,
+				courseList, numCourse,
+				studentList, numStudents,
+				scheduleList, numSchedules);
+			break;
+		default:
+			break;
+		}
+	}
+
+}
+
+
+
+
--- registrationSystemProgram/registrationSystemProgram/Menu.h
+++ registrationSystemProgram/registrationSystemProgram/Menu.h
@@ -1,129 +1,18 @@
 #pragma once
-//#include <iostream>
-bool subMenuA() {
-	std::cout << "Archivo:" << std::endl;
-	std::cout << "  (a)Acerca de\n"<<
-		"  (b)Salir " << std::endl;
-	std::cout << "Selecione una opcion:" << std::endl;
-	char option=enterChar();
-	switch (option) {
-	case 'a':
-		about();
-		return false;
-	case 'b':
-		std::cout << "Saliendo....." << std::endl;
-		return true;
-	default:
-		return false;
-	}
-}
-
-void subMenuB(Course* courseList, int& numCourse, 
-	Student* studentList, int& numStudents,
-	Schedule* scheduleList, int& numSchedules){
-	std::cout << "Mantenimiento:" << std::endl;
-	std::cout << "  (a)Estudiantes(Agrega estudiante a lista))\n" <<
-		"  (b)Cursos(Agrega curso a lista) \n" <<
-		"  (c)Horarios(Agrega horario a lista)"<<std::endl;
-	std::cout << "Selecione una opcion:" << std::endl;
-	char option = enterChar();
-	switch (option) {
-	case 'a':
-		registerStudent(studentList,numStudents);
-		break;
-	case 'b':
-		registerCourse(courseList,numCourse,scheduleList,numSchedules);
-		break;
-	case 'c':
-		registerSchedule(scheduleList, numSchedules);
-		break;
-	default:
-		break;
-	}
-}
-
-void subMenuC(Registration* registrationList,int& numRegistration,
-	Student* studentList, int numStudents, Course* courseList, int numCourse, Schedule* scheduleList, int numSchedules) {
-	std::cout << "Matricula:" << std::endl;
-	std::cout << "  (a)Registro de matricula" << std::endl;
-	std::cout << "Selecione una opcion:" << std::endl;
-	char option = enterChar();
-	switch (option) {
-	case 'a':
-		addRegistration(registrationList,numRegistration,
-			studentList,numStudents,courseList,numCourse,scheduleList,numSchedules);
-		break;
-	default:
-		break;
-	}
-}
-
-void subMenuD(Registration* registrationList,int numRegistration,
-	Course* courseList, int numCourse, 
-	Student* studentList, int numStudents,
-	Schedule* scheduleList, int numSchedules) {
-	std::cout << "Consulta:" << std::endl;
-	std::cout << "  (a)Estudiantes(Lista de estudiantes)\n" <<
-		"  (b)Cursos(Lista de los cursos)\n" <<
-		"  (c)Horarios(Lista de Horarios)\n" <<
-		"  (d)Estudiantes Matriculados(Todos los matriculados) \n" << std::endl;
-	std::cout << "Selecione una opcion:" << std::endl;
-	char option = enterChar();
-	switch (option) {
-	case 'a':
-		showStudentsList(studentList,numStudents);
-		break;
-	case 'b':
-		showCourseList(courseList, numCourse);
-		break;
-	case 'c':
-		showScheduleList(scheduleList,numSchedules);
-		break;
-	case 'd':
-		showEnrolledStudents(registrationList,numRegistration);
-		break;
-	default:
-		break;
-	}
-}
+#include <iostream>
+#include "Student.h"
+#include "Schedule.h"
+#include "Course.h"
+#include "Registration.h"
+#include "processes.h"
+#include "Menu.h"
+#include "loadPreRecords.h"
 
 
-void menu(Registration* registrationList,int& numRegistration,
-	Course* courseList,int& numCourse,
-	Student* studentList,int& numStudents,
-	Schedule* scheduleList, int& numSchedules) {
-	bool exit = false;
-	while (exit==false) {
-		std::cout << "\nBienvenido al sistema de registro o matricula" << std::endl;
-		std::cout << "  (a)Archivo\n" <<
-			"  (b)Mantenimiento \n" <<
-			"  (c)Matricula\n" <<
-			"  (d)Consulta\n" << std::endl;
-		std::cout << "Ingrese la opcion que desea realizar: " << std::endl;
-		char option = enterChar();
-		//char option =' ';
-		//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		//std::cin >> option;
-		switch (option) {
-		case 'a':
-			exit=subMenuA();
-			break;
-		case 'b':
-			subMenuB(courseList, numCourse, studentList, numStudents,scheduleList,numSchedules);
-			break;
-		case 'c':
-			subMenuC(registrationList, numRegistration,
-				studentList, numStudents, courseList, numCourse, scheduleList, numSchedules);
-			break;
-		case 'd':
-			subMenuD(registrationList,numRegistration,
-				courseList,numCourse,
-				studentList,numStudents,
-				scheduleList,numSchedules);
-			break;
-		default:
-			break;
-		}
-	}
-
-}
+#include "MethodStatement.h"
+bool subMenuA();
+void subMenuB(Course* courseList, int& numCourse, Student* studentList, int& numStudents, Schedule* scheduleList, int& numSchedules);
+void subMenuC(Registration* registrationList, int& numRegistration, Student* studentList, int numStudents, Course* courseList, int numCourse, Schedule* scheduleList, int numSchedules);
+void subMenuD(Registration* registrationList, int numRegistration,Course* courseList, int numCourse,Student* studentList, int numStudents,Schedule* scheduleList, int numSchedules);
+void menu(Registration* registrationList, int& numRegistration, Course* courseList, int& numCourse, Student* studentList, int& numStudents, Schedule* scheduleList, int& numSchedules);
+	
