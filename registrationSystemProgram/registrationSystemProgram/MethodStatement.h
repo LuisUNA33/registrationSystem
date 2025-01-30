4ffaf8a10f0ae466443b254cc9de7c3b7368ce90
LuisSanchezAlvarez <luis.sanchez.alvarez@est.una.ac.cr>
2025-01-30 10:53:23 -0600

    other files

---
+++ registrationSystemProgram/registrationSystemProgram/MethodStatement.h
@@ -0,0 +1,6 @@
+#pragma once
+
+//void holi();
+void crearArhivotxt(std::string nombreArchivo);
+void agregarLineaArchivotxt(std::string nombreArchivo, std::string texto);
+std::string leerArchivo(std::string nombreArchivo);
