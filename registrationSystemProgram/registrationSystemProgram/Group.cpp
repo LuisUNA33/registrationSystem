361c23c07bed97ed324a0b9c3e1879306487b697
Mathew <163693332+mathewramirez@users.noreply.github.com>
2025-02-09 16:02:25 -0600

    update class Group
    gets and sets of the class

--- registrationSystemProgram/registrationSystemProgram/Group.cpp
+++ registrationSystemProgram/registrationSystemProgram/Group.cpp
@@ -1,9 +1,50 @@
 #include "Group.h"
 
+Group::Group()
+{
+}
+
 Group::Group(std::string ncr, std::string codCourse, std::string professor){
-	this->ncr = ncr;
-
+	this->NCR = NCR;
 	this->codCourse=codCourse;
-
 	this->professor = professor;
+}
+
+std::string Group::getNCR()
+{
+	return this->NCR;
+}
+
+std::string Group::getCodeCourses()
+{
+	return this->codCourse;
+}
+
+std::string Group::getProfessor()
+{
+	return this->professor;
+}
+
+void Group::setNCR(std::string NCR)
+{
+	this->NCR = NCR;
+}
+
+void Group::setCodeCourse(std::string codeCourse)
+{
+	this->codCourse = codeCourse;
+}
+
+void Group::setProfessor(std::string Professor)
+{
+	this->professor = professor;
+}
+void Group::showGroup() {
+	std::cout << "\nGrupo:" << std::endl;
+	std::cout << "NCR: ";
+	std::cout << this->NCR << std::endl;
+	std::cout << "Codigo de curso: ";
+	std::cout << this->codCourse << std::endl;
+	std::cout << "Profesor: ";
+	std::cout << this->professor << std::endl;
 }
--- registrationSystemProgram/registrationSystemProgram/Group.h
+++ registrationSystemProgram/registrationSystemProgram/Group.h
@@ -5,13 +5,20 @@
 class Group
 {
 private:
-    std::string ncr = "";
+    std::string NCR = "";
     std::string codCourse = "";
     std::string professor = "";
 
 public:
     Group();
     Group(std::string ncr, std::string codCourse, std::string professor);
+    std::string getNCR();
+    std::string getCodeCourses();
+    std::string getProfessor();
+    void setNCR(std::string NCR);
+    void setCodeCourse(std::string codeCourse);
+    void setProfessor(std::string Professor);
+    void showGroup();
 
 };
 #endif
