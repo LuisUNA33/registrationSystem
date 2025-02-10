0dfbbbe708e75ce8887c260856762526c241091c
Mathew <163693332+mathewramirez@users.noreply.github.com>
2025-02-09 18:32:16 -0600

    update

--- registrationSystemProgram/registrationSystemProgram/Requirement.cpp
+++ registrationSystemProgram/registrationSystemProgram/Requirement.cpp
@@ -4,7 +4,7 @@ Requirement::Requirement()
 {
 }
 
-Requirement::Requirement(std::string CodeCourse, std::string Requirements_A, std::string Requirements_B)
+Requirement::Requirement(std::string codeCourse, std::string requirement_A, std::string requirement_B)
 {
 	this->codeCourse = codeCourse;
 	this->requirement_A = requirement_A;
@@ -21,7 +21,7 @@ std::string Requirement::getRequirement_B()
 	return this->requirement_B;
 }
 
-std::string Requirement::codCourse()
+std::string Requirement::getCodCourse()
 {
 	return this->codeCourse;
 }
@@ -41,7 +41,14 @@ void Requirement::setCodCourse(std::string codeCourse)
 	this->codeCourse = codeCourse;
 }
 
-void Requirement::showRequirements()
+void Requirement::showRequirement()
 {
-	
+	std::cout << "\nRequerimientos " << std::endl;
+	std::cout << "codigo de curso: ";
+	std::cout << this->codeCourse<< std::endl;
+	std::cout << "requerimiento 1: ";
+	std::cout << this->requirement_A<< std::endl;
+	std::cout << "requerimiento 2: ";
+	std::cout << this->requirement_B << std::endl;
+
 }
--- registrationSystemProgram/registrationSystemProgram/Requirement.h
+++ registrationSystemProgram/registrationSystemProgram/Requirement.h
@@ -13,11 +13,11 @@ public:
 	Requirement(std::string CodeCourse, std::string Requirements_A, std::string Requirements_B);
 	std::string getRequirement_A();
 	std::string getRequirement_B();
-	std::string codCourse();
+	std::string getCodCourse();
 	void setRequirement_A(std::string requirement_A);
 	void setRequirement_B(std::string requirement_B);
 	void setCodCourse(std::string codeCourse);
-	void showRequirements();
+	void showRequirement();
 
 };
 
