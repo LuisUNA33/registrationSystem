b837dbf53b58cfb01422b35fa3c7c90ed50f46a3
John Perez <john.perez.quesada@est.una.ac.cr>
2025-02-10 11:59:16 -0600

    update

--- registrationSystemProgram/registrationSystemProgram/registrationSystemProgram.cpp
+++ registrationSystemProgram/registrationSystemProgram/registrationSystemProgram.cpp
@@ -1,25 +1,4 @@
-#include <iostream>
-#include <string>
-using namespace std;
-#include "Student.h"
-#include "NodeStudent.h"
-#include "Schedule.h"
-#include "NodeSchedule.h"
-#include "Course.h"
-#include "NodeCourse.h"
-#include "Group.h"
-#include "NodeGroup.h"
-
-#include "Requirement.h"
-#include "NodeRequirement.h"
-
-#include "ApprovedCourse.h"
-#include "NodeApprovedCourse.h"
-
-#include "Files.h"
-#include "Processes.h"
 #include "Menu.h"
-
 
 int main() {
     menu();
